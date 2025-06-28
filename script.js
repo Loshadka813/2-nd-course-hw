3113e3e7abcbcafe596e050b82f38eba22688b0f
Loshadka813 <marina.golubkova.2017@mail.ru>
2025-06-28 16:06:48 +0400

    hw-6

--- script.js
+++ script.js
@@ -31,3 +31,54 @@ function guessNumber() {
     }
 }
 
+
+function arithmetic() {
+    const num1 = Math.floor(Math.random() * 20) + 1;
+    const num2 = Math.floor(Math.random() * 20) + 1;
+    const operators = ['+', '-', '*', '/'];
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+
+    let result;
+
+    if (operator === '/' && num2 === 0) {
+        return arithmetic();
+    }
+
+    if (operator === '/' && num1 % num2 !== 0) {
+        return arithmetic();
+    }
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+
+    const userAnswer = prompt(`Сколько будет ${num1} ${operator} ${num2} = ?`);
+
+    if (userAnswer === null) {
+        alert('Игра завершена');
+        return;
+    }
+
+    const answer = parseFloat(userAnswer);
+
+    if (isNaN(answer)) {
+        alert('Введите число.');
+    } else if (answer === result) {
+        alert('Правильно!');
+    } else {
+        alert(`Неправильно. Правильный ответ ${result}`);
+    }
+}
+
+
